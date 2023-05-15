4237e7f462ead6c54817680ba3237c26492264ae
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-05-15 14:41:02 +0700

    Initial Commit

---
+++ KorlantasReport/KorlantasReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  KorlantasReportApp.swift
+//  KorlantasReport
+//
+//  Created by MacBook Pro on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct KorlantasReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
