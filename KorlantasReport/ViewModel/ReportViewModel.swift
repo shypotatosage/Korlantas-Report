c96352a403da35d4f3a83ea5ef80dc9bbe443892
HansJoachimW <hansjw123@gmail.com>
2023-05-27 21:34:33 +0700

    Update ReportViewModel
    Changing the main submit function to send a POST request

--- KorlantasReport/ViewModel/ReportViewModel.swift
+++ KorlantasReport/ViewModel/ReportViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+let postURL = "http://127.0.0.1:8000/api/submit-report"
+
 class ReportViewModel: ObservableObject{
     @Published var reports: [Report] = []
     
@@ -44,7 +46,33 @@ class ReportViewModel: ObservableObject{
     }
     
     func submitReport(title: String, location: String, datetime: String, description: String, image: String){
-            reports.append(Report(title: title,location: location,datetime: datetime, description: description, image: image, status: "Pending", user: User.sampleUser))
+        guard let url =  URL(string: postURL) else { return }
+        
+        let encoder = JSONEncoder()
+        
+        let newReport = Report(title: title,location: location,datetime: datetime, description: description, image: image, status: "Pending", user: User.sampleUser)
+        
+        let finalData = try! encoder.encode(newReport)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalData
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { (data, res, err) in
+            do {
+                if let data = data {
+                    let result = try JSONDecoder().decode(Report.self, from: data)
+                    print(result)
+                } else {
+                    print("No data")
+                }
+            } catch (let error) {
+                print("error: ", error.localizedDescription)
+            }
+        }
+        
+//            reports.append(Report(title: title,location: location,datetime: datetime, description: description, image: image, status: "Pending", user: User.sampleUser))
     }
 }
 
